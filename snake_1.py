b0ad1a97e76f3bc231119e8034993ddc541eba9b
changang-4life <163348160+changang-4life@users.noreply.github.com>
2026-02-13 09:13:41 +1300

    V1

---
+++ snake_1.py
@@ -0,0 +1,14 @@
+import pygame
+import time
+
+pygame.init() # initialises (starts) all pygame modules so they are ready
+
+# creating the screen / surface:
+screen = pygame.display.set_mode((1000, 720))
+time.sleep(5)
+
+pygame.quit()
+quit()
+
+
+
